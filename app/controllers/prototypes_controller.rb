61876f3e0a89d6224a830c6a9c0d1f38129ce31c
aya0216 <ayakawabata0216@gmail.com>
2021-11-27 18:01:15 +0900

    prototypeコントローラーの再編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :destroy, :update]
-  before_action :move_to_index, only: [:edit, :update]
+   before_action :set_prototype, except: [:index, :new, :create]
+   before_action :authenticate_user!, except: [:index, :show]
+   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.all
@@ -51,9 +52,11 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in? == current_user.name
-      redirect_to root_path
-    end
-  end
+   def set_prototype
+     @prototype = Prototype.find(params[:id])
+   end
+
+   def contributor_confirmation
+     redirect_to root_path unless current_user == @prototype.user
+   end
 end
